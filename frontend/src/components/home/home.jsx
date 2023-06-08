114500f2816794480731b21015e15cbf6829e70f
阮连军 <ruanlianjun@pdx.local>
2023-06-08 18:32:41 +0800

    scrool

--- frontend/src/components/home/home.jsx
+++ frontend/src/components/home/home.jsx
@@ -1,8 +1,117 @@
-import "./home.css";
+import "./home.less";
+import Icon from "supercons";
+import {useState} from "react";
 
 export default function Home() {
+    const [activityMenu, setActivityMenu] = useState("movie");
+    const clickMenu = (menu) => {
+        setActivityMenu(menu);
+    }
+
 
     return (
-        <div className={"home_detail"}>Home</div>
+        <div className={"home_detail"}>
+            <header>
+                <div>
+                    <Icon glyph="explore" size={50} className={"home_detail_logo"}></Icon>
+                </div>
+                <div className={activityMenu==="movie"?"home_detail_menu_activity":"home_detail_menu"} onClick={(e)=>clickMenu("movie")}>
+                    <span>
+                        电影
+                    </span>
+                    <span></span>
+                </div>
+                <div className={activityMenu==="tv"?"home_detail_menu_activity":"home_detail_menu"} onClick={(e)=>clickMenu("tv")}>
+                    <span>
+                        电视剧
+                    </span>
+                    <span></span>
+                </div>
+                <div className={activityMenu==="tv_live"?"home_detail_menu_activity":"home_detail_menu"} onClick={(e)=>clickMenu("tv_live")}>
+                    <span>电视直播</span>
+                    <span></span>
+                </div>
+            </header>
+
+            <div className={"home_detail_main"}>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+                <div className={"home_detail_items"}></div>
+            </div>
+        </div>
     )
 }
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed all:frontend/dist
@@ -23,9 +24,26 @@ func main() {
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
+
+		Windows: &windows.Options{
+			WebviewIsTransparent:              true,
+			WindowIsTranslucent:               true,
+			DisableWindowIcon:                 false,
+			DisableFramelessWindowDecorations: false,
+			WebviewUserDataPath:               "",
+			Theme:                             windows.SystemDefault,
+			CustomTheme: &windows.ThemeSettings{
+				DarkModeTitleBar:   windows.RGB(20, 20, 20),
+				DarkModeTitleText:  windows.RGB(200, 200, 200),
+				DarkModeBorder:     windows.RGB(20, 0, 20),
+				LightModeTitleBar:  windows.RGB(200, 200, 200),
+				LightModeTitleText: windows.RGB(20, 20, 20),
+				LightModeBorder:    windows.RGB(200, 200, 200),
+			},
+		},
 		CSSDragProperty:  "widows",
-		CSSDragValue:     "0",
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		CSSDragValue:     "1",
+		BackgroundColour: &options.RGBA{R: 0, G: 0, B: 0, A: 0},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
