1c41bcd371c94e9ccba910ecffd058a37ca436df
阮连军 <ruanlianjun@pdx.local>
2023-06-09 16:52:05 +0800

    页面展示

--- main.go
+++ main.go
@@ -18,9 +18,9 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "",
-		Width:  1024,
-		Height: 768,
+		Title:     "",
+		MinWidth:  1200,
+		MinHeight: 600,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
