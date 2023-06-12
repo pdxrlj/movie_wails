613c9ad9cc774d96d6a7528696349314d8b6e970
ruanlianjun <1191073896@qq.com>
2023-06-12 22:14:07 +0800

    page

--- frontend/src/components/movie/movie.jsx
+++ frontend/src/components/movie/movie.jsx
@@ -4,16 +4,15 @@ import {useEffect, useRef, useState} from "react";
 import axios from "axios";
 
 export default function Movie() {
-    const [movieLists,setMovieLists]=useState([])
+    const [movieLists, setMovieLists] = useState([])
     const scrollRef = useRef(null);
-    const [page,setPage]=useState(1)
 
-    useEffect(()=>{
-         axios.get(`http://127.0.0.1:8080/api/v1/movie/discover?page=${page}`).then((res)=>{
+    useEffect(() => {
+        axios.get(`http://127.0.0.1:8080/api/v1/movie/discover?page=1`).then((res) => {
             setMovieLists(res.data.results)
         })
-        setPage(page+1)
-    },[])
+        console.log("======================================1")
+    }, [])
 
 
     useEffect(() => {
@@ -22,46 +21,77 @@ export default function Movie() {
             return;
         }
 
-        node.addEventListener('scroll', (event) => {
-            if (node.scrollHeight - node.scrollTop === node.clientHeight) {
-                axios.get(`http://127.0.0.1:8080/api/v1/movie/discover?page=${page}`).then((res)=>{
-                    setMovieLists(res.data.results)
+        debounce(node.addEventListener('scroll', (event) => {
+            console.log("开始滚动了:", node.scrollHeight - node.scrollTop, node.clientHeight)
+            if (node.scrollHeight - node.scrollTop <= node.clientHeight) {
+                const CancelToken = axios.CancelToken;
+                const source = CancelToken.source();
+                if (movieLists.page+1==="NaN"){
+                    return;
+                }
+                let url = `http://127.0.0.1:8080/api/v1/movie/discover?page=${movieLists.page + 1}`
+                console.log("请求url:", url)
+                axios.get(url,{cancelToken: source.token}).then((res) => {
+                    setMovieLists([...movieLists, ...res.data.results])
+                    console.log("到底了", movieLists)
                 })
-                setMovieLists(movieLists.concat(movieLists))
-                setPage(page+1)
+
+
             }
-        });
+        }), 1000)
 
         return () => {
-            node.removeEventListener('scroll', () => {});
+            node.removeEventListener('scroll', () => {
+            });
         };
-    }, []);
+    }, [movieLists.page]);
 
-    return (
-        <div className={"movie_content"} ref={scrollRef}>
-            { movieLists.map((movie,index)=>{
-                return (
-                    <div className={"movie_detail_items"} key={index}>
-                        <div className={"movie_detail_item_cover"}>
-                            <img src={`https://image.tmdb.org/t/p/w342/${movie.poster_path}`} alt="变形金刚"/>
-                        </div>
-                        <div className={"movie_detail_item_desc"}>
-                            { movie.overview }
-                        </div>
-                        <div className={"movie_detail_items_tags"}>
+
+    const debounce = (fn, delay) => {
+        let timeout;
+        return function () {
+            clearTimeout(timeout)
+            timeout = setTimeout(() => {
+                fn()
+            }, delay)
+        }
+    }
+
+    const throttle = (fn, delay) => {
+        let timer;
+        return function () {
+            if (!timer) {
+                fn.apply(this, arguments)
+                timer = setTimeout(() => {
+                    clearTimeout(timer)
+                    timer = null
+                }, delay)
+            }
+        }
+    }
+
+
+    return (<div className={"movie_content"} ref={scrollRef}>
+            {movieLists.results?.map((movie, index) => {
+                return (<div className={"movie_detail_items"} key={index}>
+                    <div className={"movie_detail_item_cover"}>
+                        <img src={`https://image.tmdb.org/t/p/w342/${movie.poster_path}`} alt="变形金刚"/>
+                    </div>
+                    <div className={"movie_detail_item_desc"}>
+                        {movie.overview}
+                    </div>
+                    <div className={"movie_detail_items_tags"}>
                             <span>
                                 <Icon glyph={"emoji"} size={17} className={"movie_detail_items_tags_logo"}></Icon>
                                 {movie.title}
                             </span>
-                            <span>
+                        <span>
                                 <Icon glyph={"clock"} size={17} className={"movie_detail_items_tags_time"}></Icon>
                                 <span className={"movie_detail_items_tags_time_time"}>{movie.release_date}</span>
                             </span>
-                        </div>
                     </div>
-                )
+                </div>)
             })}
-
 
 
         </div>
